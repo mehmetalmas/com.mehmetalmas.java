93684dabe612a7a320226a3b46d8d4bd5d8c10da
Mehmet Almas <m.almas63@gmail.com>
2023-12-15 18:27:04 +0100

    151223

---
+++ src/main/java/M01_Scope.java
@@ -0,0 +1,23 @@
+public class M01_Scope {
+
+    int a;
+    int b;
+
+
+    public static void main(String[] args) {
+
+        int a;
+        int b;
+
+
+        System.out.println("abc");
+
+    }
+
+    public static double pi(){
+        return 3.14;
+    }
+
+
+
+}
